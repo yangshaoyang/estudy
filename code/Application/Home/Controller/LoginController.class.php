7258cd27296fa85eb1d94e4229c5c58ca4c8f83c
huqiArie <736509710@qq.com>
2016-11-15 10:47:19 +0800

    励志+注册登录

---
+++ code/Application/Home/Controller/LoginController.class.php
@@ -0,0 +1,8 @@
+<?php
+namespace Home\Controller;
+use Think\Controller;
+class NoteController extends Controller {
+    public function login(){
+        $this->display();
+    }
+}
---
+++ code/Application/Home/Controller/NoteController.class.php
@@ -0,0 +1,11 @@
+<?php
+namespace Home\Controller;
+use Think\Controller;
+class NoteController extends Controller {
+    public function note(){
+        $this->display();
+    }
+    public function notedetails(){
+        $this->display();
+    }
+}
