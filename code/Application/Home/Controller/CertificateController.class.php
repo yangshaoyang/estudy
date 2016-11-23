a0c733491b8ee0a5a21b2a44bca4bef8d9b1f66c
huqiArie <736509710@qq.com>
2016-11-23 09:16:58 +0800

    考证页面

---
+++ code/Application/Home/Controller/CertificateController.class.php
@@ -0,0 +1,11 @@
+<?php
+namespace Home\Controller;
+use Think\Controller;
+class CertificateController extends Controller {
+    public function certificatelist(){
+        $this->display();
+    }
+    public function ccontent(){
+        $this->display();
+    }
+}
