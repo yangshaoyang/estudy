d7c3b15e1835a3482a9e16202650fa4645bded23
yangshaoyang <y@yangshaoyang.cn>
2016-11-27 18:29:23 +0800

    比赛提示部分动态获取

--- code/Application/Home/Controller/MatchController.class.php
+++ code/Application/Home/Controller/MatchController.class.php
@@ -2,7 +2,18 @@
 namespace Home\Controller;
 use Think\Controller;
 class MatchController extends Controller {
-    public function matchlist(){
-        $this->display();
-    }
+	public function index(){
+		$this->display();
+	}
+	 public function matchlist(){
+	      $match=M("match")->select();
+	      $this->assign("match",$match);
+	      $this->display();
+	}
+	public function content($mid){
+		$match=M("match")->find($mid);
+		$this->assign("mcontent",$match);
+		$this->display();
+	}
+
 }
