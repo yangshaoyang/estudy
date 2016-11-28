588b28e54badf62c0a4d575a4cbb8395a8a83afc
yangshaoyang <y@yangshaoyang.cn>
2016-11-28 19:49:29 +0800

    考证页面动态获取

--- code/Application/Home/Controller/CertificateController.class.php
+++ code/Application/Home/Controller/CertificateController.class.php
@@ -2,10 +2,20 @@
 namespace Home\Controller;
 use Think\Controller;
 class CertificateController extends Controller {
-    public function certificatelist(){
-        $this->display();
-    }
-    public function ccontent(){
-        $this->display();
-    }
+	public function index(){
+		$this->display();
+	}
+	 public function certificatelist(){
+	      $certificate=M("certificate")->select();
+	      $this->assign("certificate",$certificate);
+	      $this->display();
+	}
+	public function content($cid){
+		$certificate=M("certificate")->find($cid);
+		$this->assign("ccontent",$certificate);
+		$tcertificate=M("certificate")->limit(3)->select();//比赛推荐部分
+	      $this->assign("tcertificate",$tcertificate);
+		$this->display();
+	}
+
 }
