c731db41465a73a5c845a0410171e71d0f747f54
yangshaoyang <y@yangshaoyang.cn>
2016-11-29 19:22:37 +0800

    后台登录静态页的实现
    Signed-off-by: yangshaoyang <y@yangshaoyang.cn>

---
+++ code/Application/Admin/Controller/ChangepasswordController.class.php
@@ -0,0 +1,11 @@
+<?php
+namespace Admin\Controller;
+use Think\Controller;
+class ChangepasswordController extends Controller {
+    public function login(){
+        $this->display();
+    }
+    public function index(){
+        $this->display();
+    }
+}
---
+++ code/Application/Admin/Controller/LoginController.class.php
@@ -0,0 +1,11 @@
+<?php
+namespace Admin\Controller;
+use Think\Controller;
+class LoginController extends Controller {
+    public function login(){
+        $this->display();
+    }
+    public function index(){
+        $this->display();
+    }
+}
