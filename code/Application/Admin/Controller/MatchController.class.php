12f6691ecf51cc23cd13d795193fef6d9a54bd07
yangshaoyang <y@yangshaoyang.cn>
2016-12-01 17:57:17 +0800

    添加比赛部分 控制器

--- code/Application/Admin/Controller/MatchController.class.php
+++ code/Application/Admin/Controller/MatchController.class.php
@@ -3,7 +3,8 @@ namespace Admin\Controller;
 use Think\Controller;
 class MatchController extends Controller {
     public function index(){
-        $match = M("match")->select();
+        $match = M("match")->
+select();
 	  $this->assign("match",$match);
 	  $this->display();
     }
@@ -13,11 +14,30 @@ class MatchController extends Controller {
 	  $this->display();
 	}
     public function edit($mid){
-	  $match=M("match")->find($mid);
-	  $this->assign("mcontent",$match);
-	  $this->display();
-	}
-    public function addmatch(){
+        $this->display();
+    }
+    public function add(){
+    	if(!IS_POST){
+            exit("bad request");
+        }
+         $data=array(
+    			'mname' =>I('post.mname'),
+    			'mtime'  =>I('post.mtime'),
+    			'mrequest'=>I('post.mrequest'),
+    			'mfee'    =>I('post.mfee'),
+    			'murl'   =>I('post.murl'),
+    			'mcontent'  =>I('post.mcontent')
+    			);
+
+        $Model=D("match");
+        $Model->create();
+        $num=$Model->add($data);
+        if($num>0){
+        	$this->success("添加成功！",U("bid"));
+        }else{
+        	$this->error("添加失败！",U("bid"));
+        }
+
         $this->display();
     }
     public function editmatch($mid){
