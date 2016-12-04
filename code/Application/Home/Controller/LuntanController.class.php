5dcc6963ed75ca55e67667af2568dbf4a0e050fa
lixiaoyu1002 <920871718@qq.com>
2016-12-04 16:27:33 +0800

    我要发帖

--- code/Application/Home/Controller/LuntanController.class.php
+++ code/Application/Home/Controller/LuntanController.class.php
@@ -3,6 +3,7 @@ namespace Home\Controller;
 use Think\Controller;
 use \Library\Page;
 class LuntanController extends Controller {
+	//论坛list页的控制器
     public function index(){
     	/*选取数据*/
 	    	$forum=M("forum")->where("typeid=1");
@@ -10,14 +11,14 @@ class LuntanController extends Controller {
 		// 1. 获取记录总条数
         $count =$forum->count();
         // 2. 设置（获取）每一页显示的个数
-        $pageSize =7;
+        $pageSize =5;
         // 3. 创建分页类对象
         $page = new Page($count, $pageSize);
       //4.构造查询条件
         $condition=array();
         $condition['typeid']=1;
         // 5. 分页查询
-        $forum = $forum->where($condition)
+        $forum = $forum->where($condition)->order('forumid desc')
             ->limit($page->firstRow.','.$page->listRows)
             ->select();
         //6定义分页样式
@@ -27,18 +28,40 @@ class LuntanController extends Controller {
         $this->assign('forum', $forum);
         $pages=$page->show();
         $this->assign('pages',$pages);
+         /*热议榜数据*/
+	    $forum_hot=M("forum")->order('readcount desc')->limit(5)->select();
+        $this->assign('forum_hot', $forum_hot);
         $this->display();
     }
+
+    //论坛内容页的控制器
     public function questions($forumid){
+    	//找id对应的帖子内容
     	$forum=M("forum");
-    	/*文章阅读量+1*/
-		$forum->where('forumid=1')->setInc('readcount');
+    		//阅读量
+				$forum->where("forumid=$forumid")->setInc('readcount');
     	$forum=$forum->find($forumid);
 		$this->assign("fcontent",$forum);
 		
-		/*$tmatch=M("match")->limit(3)->select();//比赛推荐部分
-	      $this->assign("tmatch",$tmatch);
-		$this->display();*/
+		//分类名称
+			//1.构造查询条件
+			$condition=array();
+			$condition['typeid']=M("forum")->where("forumid=$forumid")->getField('typeid');
+			//2.查询分类名称
+			$typename=M("forum_sort")->where($condition)->getField('typename');
+			//dump($typename);
+			$this->assign("typename",$typename);
+
+		//发帖人名称
+			//1.构造查询条件
+			$condition=array();
+			$condition['userid']=M("forum")->where("forumid=$forumid")->getField('userid');
+			//2.查询分类名称
+			$username=M("users")->where($condition)->getField('username');
+			//dump($typename);
+			$this->assign("username",$username);
+
+        //输出结果
         $this->display();
     }
 }
