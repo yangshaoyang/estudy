938762baa290d3d2b6b7327a4e0b02cb69461b43
xuwenyue <15230172230@163.com>
2016-12-04 22:21:19 +0800

    更新controller

--- code/Application/Home/Controller/PersonController.class.php
+++ code/Application/Home/Controller/PersonController.class.php
@@ -7,10 +7,25 @@ class PersonController extends Controller {
     }
 
     public function homepage(){
+        $map['userid']=1;
+        $users=M("users")->where($map)->select();
+        //dump($users);
+        $user_certificate=M("user_certificate")->select();
+        $user_match=M("user_match")->select();
+        $this->assign("users",$users[0]);
+        $this->assign("user_certificate",$user_certificate);
+        $this->assign("user_match",$user_match);
         $this->display();
     }
     public function message(){
-        $this->display();
+        //$this->display();
+          $user_certificate=M("user_certificate")->select();
+          $user_match=M("user_match")->select();
+          $this->assign("user_certificate",$user_certificate);
+          //$this->display();
+          
+          $this->assign("user_match",$user_match);
+          $this->display();
     }
     public function permessage(){
         $this->display();
