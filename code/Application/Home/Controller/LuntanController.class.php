3c830bc676acc7566d90b1c7e49e26c2b2e33e3c
lixiaoyu1002 <920871718@qq.com>
2016-12-05 09:19:40 +0800

    论坛更新

--- code/Application/Home/Controller/LuntanController.class.php
+++ code/Application/Home/Controller/LuntanController.class.php
@@ -5,11 +5,11 @@ use \Library\Page;
 class LuntanController extends Controller {
 	//论坛list页的控制器
     public function index(){
-    	/*选取数据*/
-	    	$forum=M("forum")->where("typeid=1");
+    	/*选取分类1数据*/
+	    	$forum1=M("forum")->where("typeid=1");
 	    /*分页码*/
 		// 1. 获取记录总条数
-        $count =$forum->count();
+        $count =$forum1->count();
         // 2. 设置（获取）每一页显示的个数
         $pageSize =5;
         // 3. 创建分页类对象
@@ -18,18 +18,43 @@ class LuntanController extends Controller {
         $condition=array();
         $condition['typeid']=1;
         // 5. 分页查询
-        $forum = $forum->where($condition)->order('forumid desc')
+        $forum1 = $forum1->where($condition)->order('forumid desc')
             ->limit($page->firstRow.','.$page->listRows)
             ->select();
         //6定义分页样式
         $page->setConfig('prev','上一页');
         $page->setConfig('next','下一页');
         // 7. 输出查询结果
-        $this->assign('forum', $forum);
+        $this->assign('forum', $forum1);
         $pages=$page->show();
         $this->assign('pages',$pages);
+
+        /*选取分类2数据*/
+            $forum2=M("forum")->where("typeid=2");
+        /*分页码*/
+        // 1. 获取记录总条数
+        $count =$forum2->count();
+        // 2. 设置（获取）每一页显示的个数
+        $pageSize =5;
+        // 3. 创建分页类对象
+        $page = new Page($count, $pageSize);
+      //4.构造查询条件
+        $condition=array();
+        $condition['typeid']=2;
+        // 5. 分页查询
+        $forum2 = $forum2->where($condition)->order('forumid desc')
+            ->limit($page->firstRow.','.$page->listRows)
+            ->select();
+        //6定义分页样式
+        $page->setConfig('prev','上一页');
+        $page->setConfig('next','下一页');
+        // 7. 输出查询结果
+        $this->assign('forum2', $forum2);
+        $pages=$page->show();
+        $this->assign('pages',$pages);
+
          /*热议榜数据*/
-	    $forum_hot=M("forum")->order('readcount desc')->limit(5)->select();
+        $forum_hot=M("forum")->order('readcount desc')->limit(5)->select();
         $this->assign('forum_hot', $forum_hot);
         $this->display();
     }
