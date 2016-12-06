2185bfe55ded4f5bd782899076bfd229d4c9e9e8
zhouyukai110 <827072652@qq.com>
2016-12-06 14:28:11 +0800

    登陆后注销

--- code/Application/Home/Controller/LoginController.class.php
+++ code/Application/Home/Controller/LoginController.class.php
@@ -1,27 +1,8 @@
 <?php
 namespace Home\Controller;
-use Think\Session\Driver;
 use Think\Controller;
-
 class LoginController extends Controller {
     public function login(){
         $this->display();
-    }
-    public function validation(){
-        $data = I('post.');
-    	$User = M('users');
-        $email = $data['yourname'];
-        $password = md5($data['yourpassword']);
-        //dump($password);
-        $result = $User->getFieldByEmail($email,'password');
-        dump($result);
-        $username = $User->getFieldByEmail($email,'username');
-        if ($result == $password) {
-        	session('name',$username); 
-        	$this->redirect('/');
-        	
-        }else{
-        	$this->error('用户名或密码不正确',U('home/login/login'));
-        }
     }
 }
