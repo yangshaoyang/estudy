6a412f9e7d74df3b050a1d299697265ead4235ab
zhouyukai110 <827072652@qq.com>
2016-12-06 15:46:43 +0800

    登录控制更改

--- code/Application/Home/Controller/LoginController.class.php
+++ code/Application/Home/Controller/LoginController.class.php
@@ -5,4 +5,25 @@ class LoginController extends Controller {
     public function login(){
         $this->display();
     }
+    public function validation(){
+        $data = I('post.');
+    	$User = M('users');
+        $email = $data['yourname'];
+        $password = md5($data['yourpassword']);
+        //dump($password);
+        $result = $User->getFieldByEmail($email,'password');
+        //dump($result);
+        $username = $User->getFieldByEmail($email,'username');
+        if ($result == $password) {
+        	session('name',$username); 
+        	$this->redirect('/');
+        	
+        }else{
+        	$this->error('用户名或密码不正确',U('home/login/login'));
+        }
+    }
+    public function exits(){
+        session('name',null); 
+        $this->redirect('/');
+    }
 }
