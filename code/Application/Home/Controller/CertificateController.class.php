5688a2a7d2df17dac0f833a6b7374e98d50ac2ba
yangshaoyang <y@yangshaoyang.cn>
2016-12-06 14:21:16 +0800

    Certificate/certificatelist.html

--- code/Application/Home/Controller/CertificateController.class.php
+++ code/Application/Home/Controller/CertificateController.class.php
@@ -1,13 +1,37 @@
 <?php
 namespace Home\Controller;
 use Think\Controller;
+use \Library\Page;
 class CertificateController extends Controller {
 	public function index(){
 		$this->display();
 	}
 	 public function certificatelist(){
-	      $certificate=M("certificate")->select();
-	      $this->assign("certificate",$certificate);
+	 	/*选取分类1数据*/
+	 	$certificatepage=M("certificate")->where("cid");
+	 	/*分页码*/
+		// 1. 获取记录总条数
+		$count =$certificatepage->count();
+		// 2. 设置（获取）每一页显示的个数
+		$pageSize =8;
+		// 3. 创建分页类对象
+		$page = new Page($count, $pageSize);
+		// 4.构造查询条件
+		$condition=array();
+		$condition['cid'] != NULL;
+		// 5. 分页查询
+	      $certificatepage = $certificatepage->where($condition)->order('cid desc')
+	          ->limit($page->firstRow.','.$page->listRows)
+	          ->select();
+	      // 6. 定义分页样式
+	      $page->setConfig('prev','上一页');
+	      $page->setConfig('next','下一页');
+	      // 7. 输出查询结果
+	      $this->assign('certificate', $certificatepage);//遍历数据数据
+	      $pages=$page->show();
+	      $this->assign('pages',$pages);
+	      //$certificate=M("certificate")->select();
+	      //$this->assign("certificate",$certificate);
 	      $this->display();
 	}
 	public function content($cid){
