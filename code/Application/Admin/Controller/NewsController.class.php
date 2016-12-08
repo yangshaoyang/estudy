2e970b61d6da83b963f788ca9c83fe8455a4fa56
zhouyukai110 <827072652@qq.com>
2016-12-08 11:19:55 +0800

    添加资讯页获取

--- code/Application/Admin/Controller/NewsController.class.php
+++ code/Application/Admin/Controller/NewsController.class.php
@@ -7,7 +7,7 @@ class NewsController extends Controller {
 	   $this->assign("news",$news);
 	   $this->display();
      }
-     public function newsdetails($newsid){
+     public function newsetails($newsid){
 	   $news=M("news")->find($newsid);
 	   $this->assign("newscontent",$news);
 	   $this->display();
@@ -39,13 +39,13 @@ class NewsController extends Controller {
         $this->display();
     }
     //修改帖子
-     public function editNews($Newsid){
+     public function editNews($newsid){
 	  $news=M("news")->find($newsid);
 	   $this->assign("newscontent",$news);
 	   $this->display();
 	 }
     //删除帖子
-      public function delete($Newsid){
+      public function delete($newsid){
         $articleid = $_GET['newsid'];
         if (M("news")->delete($newsid)) {
             $this->success("删除成功！");
