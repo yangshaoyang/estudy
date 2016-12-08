60a03dfc8536adc54b08d26c364e026218176558
yangshaoyang <y@yangshaoyang.cn>
2016-12-08 20:02:01 +0800

    json包ID重命名、文件上传修改

--- code/Application/Admin/Controller/MatchController.class.php
+++ code/Application/Admin/Controller/MatchController.class.php
@@ -1,6 +1,7 @@
 <?php
 namespace Admin\Controller;
 use Think\Controller;
+use Think\Upload;
 class MatchController extends Controller {
     public function index(){
         $match = M("match")->select();
@@ -16,26 +17,48 @@ class MatchController extends Controller {
         $this->display();
     }
 
-    public function addimage(){
+    public function addimage($mid){
+        $upload = new \Think\Upload();//
+        $upload->exts      =     array('jpg', 'gif', 'png', 'jpeg');// 设置附件上传类型
+        $upload->rootPath  =     './Uploads/'; // 设置附件上传根目录
+        $upload->savePath  =     ''; // 设置附件上传（子）目录
+    // 上传文件
+        $info   =   $upload->upload();
+//dump($info);
+        $image = $info['mthumb']['savename'];
         $data = array();
         $data['statusCode']=200;
         $data['message']="上传成功";
+        $data['filename']='tttt';
+
         echo json_encode($data);
     }
     public function add(){
         if(!IS_POST){
             exit("bad request");
-        }
-        $upload = new \Think\upload();
-        $upload->rootPath   = './Public/home';
-        $upload->savePath  ='/Upload';
-        $info = $upload->upload( );
+    }
+
+        // $upload = new \Think\Upload();
+        // $upload->rootPath   = './Public/home';
+        // $upload->savePath  ='/Upload';
+        // $info = $upload->uploadOne($_FILES['image']);
+        // $image = $info['upload']['savename'];
+        // dump($image);
+        $upload = new \Think\Upload();//
+        $upload->exts      =     array('jpg', 'gif', 'png', 'jpeg');// 设置附件上传类型
+        $upload->rootPath  =     './Uploads/'; // 设置附件上传根目录
+        $upload->savePath  =     ''; // 设置附件上传（子）目录
+    // 上传文件
+        $info   =   $upload->upload();
+//dump($info);
+        $image = $info['mthumb']['savename'];
+  //       dump($image);
         $data=array(
     	'mname' =>I('post.mname'),
     	'mtime'  =>I('post.mtime'),
     	'mrequest'=>I('post.mrequest'),
     	'mfee'    =>I('post.mfee'),
-             // 'mthumb'    =>I('post.mthumb'),
+             'mthumb'    => $image,
     	'murl'   =>I('post.murl'),
     	'mcontent'  =>I('post.mcontent')
     	);
