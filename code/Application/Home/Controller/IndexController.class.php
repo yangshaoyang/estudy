aadeff14c87679e974d7cb40c088f47c856513b9
yangshaoyang <y@yangshaoyang.cn>
2016-12-11 21:57:21 +0800

    恢复

--- code/Application/Home/Controller/IndexController.class.php
+++ code/Application/Home/Controller/IndexController.class.php
@@ -3,20 +3,27 @@ namespace Home\Controller;
 use Think\Controller;
 class IndexController extends Controller {
     public function index(){
-		$match=M("match")->order("mid desc")->limit(5)->select();
-	    $this->assign("match",$match);
-	    $matchs=M("match")->order("mid desc")->limit(2)->select();
-	    $this->assign("matchs",$matchs);
+    	$match=M("match")->order("mid desc")->limit(5)->select();
+    	$this->assign("match",$match);
 
-		$textual=M("certificate")->order("cid desc")->limit(5)->select();
-	    $this->assign("textual",$textual);
-	    $textuals=M("certificate")->order("cid desc")->limit(2)->select();
-	    $this->assign("textuals",$textuals);
+    	$matchs=M("match")->order("mid desc")->limit(2)->select();
+    	$this->assign("matchs",$matchs);
 
-		$news=M("news")->order("newsid desc")->limit(5)->select();
-	    $this->assign("news",$news);
-	    $new=M("news")->order("newsid desc")->limit(1)->select();
-	    $this->assign("new",$new);
-		$this->display();
+    	$textual=M("certificate")->order("cid desc")->limit(5)->select();
+	$this->assign("textual",$textual);
+
+	$textuals=M("certificate")->order("cid desc")->limit(2)->select();
+	$this->assign("textuals",$textuals);
+
+	$news=M("news")->order("newsid desc")->limit(5)->select();
+	$this->assign("news",$news);
+
+	$new=M("news")->order("newsid desc")->limit(1)->select();
+	$this->assign("new",$new);
+
+	$url = M("blogroll")->select();
+	$this->assign("blogroll",$url);
+
+	$this->display();
     }
 }
