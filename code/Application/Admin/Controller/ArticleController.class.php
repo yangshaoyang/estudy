a95222eb12eb082e93fa9005d4416ac0eb3b3ad3
huqiArie <736509710@qq.com>
2016-12-12 23:06:17 +0800

    管理员+文章+主界面控制器

--- code/Application/Admin/Controller/ArticleController.class.php
+++ code/Application/Admin/Controller/ArticleController.class.php
@@ -44,12 +44,30 @@ class ArticleController extends Controller {
 
         $this->display();
     }
-    //修改帖子
+    //要修改文章的具体信息
      public function editarticle($articleid){
 	  $article=M("article")->find($articleid);
 	   $this->assign("articlecontent",$article);
 	   $this->display();
 	 }
+     //修改帖子
+     public function edit($articleid){
+        $data=array(
+            'articletitle'=>I('post.articletitle'),
+            'articleauthor'=>I('post.articleauthor'),
+            'articletime'=>I('post.articletime'),
+            'articleintroduction'=>I('post.articleintroduction'),
+            'articlecontent'=>I('post.articlecontent')
+            );
+        $Model=M('article');
+        $num=$Model->where('articleid='.$articleid)->save($data);
+        if($num>0){
+            $this->success("修改成功！");
+        }else{
+            $this->error("修改失败！");
+        }
+        $this-.display();
+     }
     //删除帖子
       public function delete($articleid){
         $articleid = $_GET['articleid'];
