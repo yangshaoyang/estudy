03083b8e27a085a3ce43c3fb7a30c5b635aa38c6
xuwenyue <15230172230@163.com>
2016-12-12 23:06:11 +0800

    前台个人中心控制器

--- code/Application/Home/Controller/PersonController.class.php
+++ code/Application/Home/Controller/PersonController.class.php
@@ -1,3 +1,8 @@
+<!-- 
+文件：前台个人中心控制器
+作者：徐稳越
+最后修改日期：2016年12月12日 
+-->
 <?php
 namespace Home\Controller;
 use Think\Controller;
@@ -14,7 +19,8 @@ class PersonController extends Controller {
        if ($_SESSION['name'] == NULL) {
           $this->redirect('home/login/login','请登录');
         }else{
-          //$map['userid']=1;
+          
+          //输出个人信息
           $map['username']=$_SESSION['name'];
           $users=M("users")->where($map)->select();
           $user_certificate=M("user_certificate")->order('time desc')->limit(4)->select();
@@ -23,6 +29,7 @@ class PersonController extends Controller {
           $this->assign("user_certificate",$user_certificate);
           $this->assign("user_match",$user_match);
           $this->display();
+        
       }
     }
     public function message(){
@@ -40,14 +47,35 @@ class PersonController extends Controller {
         if ($_SESSION['name'] == NULL) {
           $this->redirect('home/login/login','请登录');
         }else{
+          $map['username']=$_SESSION['name'];
+          $users=M("users")->where($map)->select();
+          $this->assign("users",$users[0]);
           $this->display();
-        }
     }
-    public function add(){
+  }
+    public function add($username){
         if ($_SESSION['name'] == NULL) {
           $this->redirect('home/login/login','请登录');
         }else{
-          $this->display();
+         $data=array(
+          'username' =>I('post.username'),
+          'sex'=>I('post.sex'),
+          'major'=>I('post.major'),
+          'colleage'=>I('post.colleage'),
+          'city'=>I('post.city'),
+          'hobby'=>I('post.hobby')
+         );
+        
+        $Model=M("users");
+        $Model->create();
+        $map[username]=$username;
+        $num=$Model->where($map)->save($data);
+        // if($num>0){
+        //   $this->success("添加成功！",U("users"));
+        // }else{
+        //   $this->error("添加失败！",U("users"));
+        // }
+          $this->redirect('home/person/homepage');
         }
     }
 
