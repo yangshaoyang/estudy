271043beb9faba4cd5d1002167694dfd181a96ea
huqiArie <736509710@qq.com>
2016-12-13 10:04:49 +0800

    后台首页控制器

--- code/Application/Admin/Controller/IndexController.class.php
+++ code/Application/Admin/Controller/IndexController.class.php
@@ -5,7 +5,7 @@ class IndexController extends Controller {
 	public function _before_index(){
 		//执行index之前会自动调用该方法
 		if($_SESSION['adminname'] == NULL){
-			$this->error('请先登录','admin/login');
+			$this->error('请先登录','admin/login/login');
 		}
 	}
     public function index(){
