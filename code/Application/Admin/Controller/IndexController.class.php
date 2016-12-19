66e55e498df9c77c7c31af03994b33df78f02799
huqiArie <736509710@qq.com>
2016-12-19 17:01:27 +0800

    后台首页代码优化

--- code/Application/Admin/Controller/IndexController.class.php
+++ code/Application/Admin/Controller/IndexController.class.php
@@ -1,9 +1,15 @@
 <?php
+/**
+*开发者：胡琪
+*开发功能：检测后台管理员是否登录
+*修改时间：2016/12/01
+*/
+
 namespace Admin\Controller;
 use Think\Controller;
 class IndexController extends Controller {
 	public function _before_index(){
-		//执行index之前会自动调用该方法
+//执行index之前会自动调用该方法
 		if($_SESSION['adminname'] == NULL){
 			$this->error('请先登录','admin/login/login');
 		}
