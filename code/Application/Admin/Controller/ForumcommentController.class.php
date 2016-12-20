e77752e7a9728d52cc7b845b0e2c4b04fd2895a8
yangshaoyang <y@yangshaoyang.cn>
2016-12-20 20:29:18 +0800

    消息推送功能

--- code/Application/Admin/Controller/ForumcommentController.class.php
+++ code/Application/Admin/Controller/ForumcommentController.class.php
@@ -12,11 +12,11 @@ class ForumCommentController extends Controller{
 		$forum_comment = M("forum_comment")->select();
 		$this->assign("forum_comment",$forum_comment);
 		$this->display();
-	}
 	//消息推送
 	//获取比赛时间的时间戳
 	$mtime = strtotime('$time,00:00:00');
 	//获取比赛三天前的时间戳
 	$btime = strtotime('-3 $time');
 
+}
 }
