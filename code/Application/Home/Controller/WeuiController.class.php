5ac72b1463c51c8facde5385484a1c1ea93885f3
yangshaoyang <y@yangshaoyang.cn>
2016-12-22 08:53:26 +0800

    微官网

--- code/Application/Home/Controller/WeuiController.class.php
+++ code/Application/Home/Controller/WeuiController.class.php
@@ -3,29 +3,30 @@ namespace Home\Controller;
 use Think\Controller;
 class WeuiController extends Controller {
     public function index(){
-    	$match=M("match")->order("mid desc")->limit(5)->select();
-    	$this->assign("match",$match);
+        $match=M("match")->order("mid desc")->limit(5)->select();
+        $this->assign("match",$match);
 
-    	$matchs=M("match")->order("mid desc")->limit(2)->select();
-    	$this->assign("matchs",$matchs);
+        $matchs=M("match")->order("mid desc")->limit(2)->select();
+        $this->assign("matchs",$matchs);
 
-    	$textual=M("certificate")->order("cid desc")->limit(5)->select();
-		$this->assign("textual",$textual);
+        $textual=M("certificate")->order("cid desc")->limit(5)->select();
+        $this->assign("textual",$textual);
 
-		$textuals=M("certificate")->order("cid desc")->limit(2)->select();
-		$this->assign("textuals",$textuals);
+        $textuals=M("certificate")->order("cid desc")->limit(2)->select();
+        $this->assign("textuals",$textuals);
 
-		$news=M("news")->order("newsid desc")->limit(5)->select();
-		$this->assign("news",$news);
+        $news=M("news")->order("newsid desc")->limit(5)->select();
+        $this->assign("news",$news);
 
-		$new=M("news")->order("newsid desc")->limit(1)->select();
-		$this->assign("new",$new);
+        $new=M("news")->order("newsid desc")->limit(1)->select();
+        $this->assign("new",$new);
 
-		$url = M("blogroll")->select();
-		$this->assign("blogroll",$url);
+        $url = M("blogroll")->select();
+        $this->assign("blogroll",$url);
 
-		$this->display();
+        $this->display();
     }
+
     public function matchlist(){
         $match=M("match")->order("mid desc")->limit(2)->select();
         $this->assign("hotmatch",$match);
@@ -35,11 +36,13 @@ class WeuiController extends Controller {
 
         $this->display();
     }
+
     public function matchcontent($mid){
         $match=M("match")->find($mid);
         $this->assign("mcontent",$match);
         $this->display();
     }
+
     public function certificatelist(){
         $certificate=M("certificate")->order("cid desc")->limit(2)->select();
         $this->assign("hotmatch",$certificate);
@@ -49,15 +52,41 @@ class WeuiController extends Controller {
 
         $this->display();
     }
+
     public function certificatecontent($cid){
         $certificate=M("certificate")->find($cid);
         $this->assign("ccontent",$certificate);
 
         $this->display();
     }
+
+    public function mclist(){
+        $match=M("match")->order("mid desc")->limit(2)->select();
+        $this->assign("hotmatch",$match);
+
+        $match=M("match")->select();
+        $this->assign("match",$match);
+
+        $certificate=M("certificate")->order("cid desc")->limit(2)->select();
+        $this->assign("hotcerticate",$certificate);
+
+        $certificate=M("certificate")->select();
+        $this->assign("certificate",$certificate);
+
+        $this->display();
+    }
+
     public function articlelist(){
         $article=M("article")->order("articleid desc")->select();
         $this->assign("article",$article);
+
+        $this->display();
+    }
+
+    public function articlecontent($aid){
+        $articleid = $aid;
+        $article=M("article")->find($articleid);
+        $this->assign("acontent",$article);
 
         $this->display();
     }
